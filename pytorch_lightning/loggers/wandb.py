2cb30243c805458b4ae3c59722cf92129a4c9c8a
Kyle Goyette <kdgoyette@gmail.com>
2021-06-10 23:16:40 -0700

    fix up

--- pytorch_lightning/loggers/wandb.py
+++ pytorch_lightning/loggers/wandb.py
@@ -257,10 +257,10 @@ class WandbLogger(LightningLoggerBase):
         # upload all checkpoints from saving dir
         if self._checkpoint_callback:
             self._scan_and_log_checkpoints(self._checkpoint_callback)
-        if status == "success":
-            wandb.finish(exit_code=0)
-        else:
-            wandb.finish(exit_code=-1)
+        #if status == "success":
+        wandb.finish(exit_code=0)
+        #else:
+        #    wandb.finish(exit_code=-1)
         self._experiment = None
 
     def _scan_and_log_checkpoints(self, checkpoint_callback: 'ReferenceType[ModelCheckpoint]') -> None:
